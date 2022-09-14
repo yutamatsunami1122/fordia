74586eb13bbd6631909aab4f415a91011ea510b9
nolozy <nolozyofficial@gmail.com>
2022-09-15 02:25:39 +0900

    app

--- __init__.py
+++ __init__.py
@@ -5,32 +5,29 @@ import os
 
 db = SQLAlchemy()
 
-def create_app():
-    app = Flask(__name__)
+app = Flask(__name__)
 
-    # app.config['SECRET_KEY'] = os.urandom(24)
-    # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+#app.config['SECRET_KEY'] = os.urandom(24)
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://fordia_user:vpQFJhZxmrXXtBIdBuKIKAqct0kLBxu2@dpg-cch0bfo2i3mukrc01ko0-a.oregon-postgres.render.com/fordia'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://fordia_user:vpQFJhZxmrXXtBIdBuKIKAqct0kLBxu2@dpg-cch0bfo2i3mukrc01ko0-a.oregon-postgres.render.com/fordia'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-    db.init_app(app)
+db.init_app(app)
 
-    login_manager = LoginManager()
-    login_manager.login_view = 'auth.login'
-    login_manager.init_app(app)
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
+login_manager.init_app(app)
 
-    from .models import User
+from .models import User
 
-    @login_manager.user_loader
-    def load_user(user_id):
-        # since the user_id is just the primary key of our user table, use it in the query for the user
-        return User.query.get(int(user_id))
+@login_manager.user_loader
+def load_user(user_id):
+    # since the user_id is just the primary key of our user table, use it in the query for the user
+    return User.query.get(int(user_id))
 
-    from .auth import auth as auth_blueprint
-    app.register_blueprint(auth_blueprint)
+from .auth import auth as auth_blueprint
+app.register_blueprint(auth_blueprint)
 
-    from .main import main as main_blueprint
-    app.register_blueprint(main_blueprint)
-
-    return app
+from .main import main as main_blueprint
+app.register_blueprint(main_blueprint)
