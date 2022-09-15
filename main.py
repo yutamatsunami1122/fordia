3388cba3efeb46a2c9de4af932a9731f182bccf4
yutamatsunami1122 <yutadmatsunami@gmail.com>
2022-09-15 19:16:53 +0900

    delete postgres URI

--- main.py
+++ main.py
@@ -1,138 +1,18 @@
-from flask import Flask, Blueprint, render_template, redirect, url_for, request, flash
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
-from datetime import datetime
-import pytz
+from flask import Blueprint, render_template, redirect, url_for, request, flash
+from flask_login import login_required, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
 import random, string
-import os
 
-db = SQLAlchemy()
+from .models import User, Friend, Chatroom, Chat, FriendRequest
 
-app = Flask(__name__)
+main = Blueprint('main',__name__)
 
-app.config['SECRET_KEY'] = os.urandom(24)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
-
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:'
-#app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db.init_app(app)
-
-login_manager = LoginManager()
-#login_manager.login_view = 'login'
-login_manager.init_app(app)
-
-class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(25))
-    email = db.Column(db.String(100), unique=True)
-    password = db.Column(db.String(1000))
-    icon = db.Column(db.String(1000))
-    friend_key = db.Column(db.String(100), unique=True)
-
-class Friend(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user1 = db.Column(db.Integer)
-    user2 = db.Column(db.Integer)
-
-class FriendRequest(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user1 = db.Column(db.Integer)
-    user2 = db.Column(db.Integer)
-
-class Chatroom(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(25))
-    chatroom_key = db.Column(db.String(100), unique=True)
-    user1 = db.Column(db.Integer)
-    user2 = db.Column(db.Integer, nullable=True)
-    user3 = db.Column(db.Integer, nullable=True)
-    user4 = db.Column(db.Integer, nullable=True)
-    user5 = db.Column(db.Integer, nullable=True)
-
-class Chat(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    chatroom_id = db.Column(db.Integer, nullable=False)
-    user_id = db.Column(db.Integer, nullable=False)
-    user_name = db.Column(db.String(25), nullable=False)
-    text = db.Column(db.String(200), nullable=False)
-    created_at = db.Column(db.DateTime, nullable=False, default=datetime.now(pytz.timezone('Asia/Tokyo')))
-
-with app.app_context():
-    db.create_all()
-
-@login_manager.user_loader
-def load_user(user_id):
-    # since the user_id is just the primary key of our user table, use it in the query for the user
-    return User.query.get(int(user_id))
-
-@app.route('/login')
-def login():
-    return render_template('login.html')
-
-@app.route('/signup')
-def signup():
-    return render_template('signup.html')
-
-@app.route('/user_settings')
-def user_settings():
-    return render_template('user_settings.html')
-
-@app.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    return redirect(url_for('index'))
-
-@app.route('/signup', methods=['POST'])
-def signup_post():
-    name = request.form.get('name')
-    email = request.form.get('email')
-    password = request.form.get('password')
-    icon = 'default_icon.jpg'
-
-    random_key = ''.join(random.choices(string.ascii_letters + string.digits, k=12))
-    friend_key = random_key
-
-    user_email = User.query.filter_by(email=email).first() # if this returns a user, then the email already exists in database
-
-    if user_email: # if a user is found, we want to redirect back to signup page so user can try again
-        flash('Email address already exists.')
-        return redirect(url_for('signup'))
-
-    # create a new user with the form data. Hash the password so the plaintext version isn't saved.
-    new_user = User(name=name, email=email, password=generate_password_hash(password, method='sha256'), icon=icon, friend_key=friend_key)
-
-    # add the new user to the database
-    db.session.add(new_user)
-    db.session.commit()
-
-    return redirect(url_for('login'))
-
-@app.route('/login', methods=['POST'])
-def login_post():
-    email = request.form.get('email')
-    password = request.form.get('password')
-    remember = True if request.form.get('remember') else False
-
-    user = User.query.filter_by(email=email).first()
-
-    # check if the user actually exists
-    # take the user-supplied password, hash it, and compare it to the hashed password in the database
-    if not user or not check_password_hash(user.password, password):
-        flash('Please check your login details and try again.')
-        return redirect(url_for('login')) # if the user doesn't exist or password is wrong, reload the page
-
-    # if the above check passes, then we know the user has the right credentials
-    login_user(user, remember=remember)
-    return redirect(url_for('mainpage'))
-
-@app.route('/')
+@main.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/mainpage')
+@main.route('/mainpage')
 @login_required
 def mainpage():
     id = current_user.id
@@ -154,7 +34,7 @@ def mainpage():
         chatrooms_user5 = chatrooms_user5,
     )
 
-@app.route('/mainpage/<string:chatroom_key>/')
+@main.route('/mainpage/<string:chatroom_key>/')
 @login_required
 def chatroom(chatroom_key):
     id = current_user.id
@@ -173,7 +53,7 @@ def chatroom(chatroom_key):
     user5 = User.query.filter_by(id=chatroom_info.user5).first()
     if not chatroom_info:
         flash('The chatroom does not exists. Please try again')
-        return redirect(url_for('mainpage'))
+        return redirect(url_for('main.mainpage'))
 
     if  current_user.id not in [chatroom_info.user1,chatroom_info.user2,chatroom_info.user3,chatroom_info.user4,chatroom_info.user5]:
         return render_template(
@@ -211,7 +91,7 @@ def chatroom(chatroom_key):
         chats = chats,
     )
 
-@app.route('/mainpage/<string:chatroom_key>/', methods=['POST'])
+@main.route('/mainpage/<string:chatroom_key>/', methods=['POST'])
 @login_required
 def chat_post(chatroom_key):
     chatroom = Chatroom.query.filter_by(chatroom_key=chatroom_key).first()
@@ -260,12 +140,12 @@ def chat_post(chatroom_key):
         chatroom_user5 = user5,
         chats = chats,
     )
-@app.route('/change_name')#change_name.html作成
+@main.route('/change_name')#change_name.html作成
 @login_required
 def change_name():
     return render_template('change_name.html')
 
-@app.route('/change_name', methods=['POST'])#名前を変更送信
+@main.route('/change_name', methods=['POST'])#名前を変更送信
 @login_required
 def change_name_update():
     user = User.query.get(current_user.id)
@@ -276,12 +156,12 @@ def change_name_update():
     flash('Updated your name')
     return render_template('change_name.html')
 
-@app.route('/change_password')
+@main.route('/change_password')
 @login_required
 def change_password():
     return render_template('change_password.html')#パスワードを変更するときのrender
 
-@app.route('/change_password', methods=['POST'])#送るとき
+@main.route('/change_password', methods=['POST'])#送るとき
 @login_required#loginしないとページを読み込まないやつ
 def change_password_update():
     user = User.query.get(current_user.id)#書いたやつを読み込む
@@ -302,13 +182,13 @@ def change_password_update():
     flash('Updated your password')
     return render_template('change_password.html')
 
-@app.route('/friend_key')#friend_key.html作成
+@main.route('/friend_key')#friend_key.html作成
 @login_required
 def friend_key():
     return render_template('friend_key.html',
     friend_key=current_user.friend_key)#friend_key取得
 
-@app.route('/friend_list')
+@main.route('/friend_list')
 @login_required
 def friend_list():
     friends1 = Friend.query.filter_by(user1=current_user.id).all()
@@ -342,7 +222,7 @@ def friend_list():
         check_f=check_f,
         check_fr=check_fr,)
 
-@app.route('/friend_list', methods=['POST'])
+@main.route('/friend_list', methods=['POST'])
 @login_required
 def friend_add():
     friend_key = request.form.get('friend_key')
@@ -350,11 +230,11 @@ def friend_add():
     friend_key_check = User.query.filter_by(friend_key=friend_key).first()
     if not friend_key_check:
         flash('The User has the friend key is not exists. Please try again')
-        return redirect(url_for('friend_list'))
+        return redirect(url_for('main.friend_list'))
 
     if current_user == friend_key_check:
         flash('This friend key is YOURS. Please try again')
-        return redirect(url_for('friend_list'))
+        return redirect(url_for('main.friend_list'))
 
     user1 = current_user.id
     user2 = User.query.filter_by(friend_key = friend_key).first()
@@ -364,9 +244,9 @@ def friend_add():
     db.session.add(friend_request)
     db.session.commit()
 
-    return redirect(url_for('friend_list'))
+    return redirect(url_for('main.friend_list'))
 
-@app.route('/maybe_to_friend', methods=['POST'])
+@main.route('/maybe_to_friend', methods=['POST'])
 @login_required
 def maybe_to_friend():
     user1 = current_user.id
@@ -380,9 +260,9 @@ def maybe_to_friend():
     db.session.delete(delete_request_friend)
     db.session.commit()
 
-    return redirect(url_for('friend_list'))
+    return redirect(url_for('main.friend_list'))
 
-@app.route('/make_chatroom_in_friend_list', methods=['POST'])
+@main.route('/make_chatroom_in_friend_list', methods=['POST'])
 @login_required
 def make_chatroom_in_friend_list():
     chatroom_user2_id = request.form.get('make_chatroom_in_friend_list')
@@ -399,9 +279,9 @@ def make_chatroom_in_friend_list():
     db.session.add(make_chatroom_in_friend_list)
     db.session.commit()
 
-    return redirect(url_for('chatroom', chatroom_key=chatroom_key))
+    return redirect(url_for('main.chatroom', chatroom_key=chatroom_key))
 
-@app.route('/chatroom_join', methods=['POST'])
+@main.route('/chatroom_join', methods=['POST'])
 @login_required
 def chatroom_join():
     chatroom_id = request.form.get('chatroom_id')
@@ -410,7 +290,7 @@ def chatroom_join():
 
     if chatroom.user5:
         flash('The members of this room are full. Search another one.')
-        return redirect(url_for('mainpage'))
+        return redirect(url_for('main.mainpage'))
 
     if chatroom.user4:
         chatroom.user5 = current_user.id
@@ -422,4 +302,4 @@ def chatroom_join():
         chatroom.user2 = current_user.id
 
     db.session.commit()
-    return redirect(url_for('mainpage'))
+    return redirect(url_for('main.mainpage'))
